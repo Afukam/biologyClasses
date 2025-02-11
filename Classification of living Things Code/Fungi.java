5df8b137e1958ce64d0c4ad11b1563af1c1868ac
Afukam <97118831+Afukam@users.noreply.github.com>
2025-02-11 05:45:48 +0000

    bugs fixed

--- Classification of living Things Code/Fungi.java
+++ Classification of living Things Code/Fungi.java
@@ -8,12 +8,12 @@ public class Fungi extends LivingThings{
 
 //no-args constructor
 
-public Fungi{
-    this.cellCount = cellCount;
-    this.parasite = parasite;
-    this.nutritionMethod = nutritionMethod;
-    this.asexual = asexual;
-    this.reproductionMethod = reproductionMethod;
+public Fungi(){
+    this.cellCount = 0;
+    this.parasite = true;
+    this.nutritionMethod = "sucker";
+    this.asexual = true;
+    this.reproductionMethod = "sexual";
 }
 
 //full-args constructor 
@@ -26,6 +26,7 @@ public Fungi(boolean movement, boolean reproduction, int nutritionNumber, boolea
     this.asexual = asexual;
     this.reproductionMethod = reproductionMethod;
 }
+
 //getters and setters
 public void setcellCount(int cellCount){
     this.cellCount = cellCount;
